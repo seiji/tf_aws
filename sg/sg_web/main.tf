7babdfb7880d04d14853af7f1b23ce7ecb464f4e
Seiji Toyama <toyama.seiji@gmail.com>
2017-02-14 13:13:20 +0900

    Add sg

---
+++ sg/sg_web/main.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "web" {
+  name = "${var.name}"
+  description = "Security Group ${var.security_group_name}"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    self = true
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "udp"
+    self = true
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.source_cidr_block}"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.source_cidr_block}"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["${var.source_cidr_block}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags { Name = "${var.name}-web" }
+}
+
